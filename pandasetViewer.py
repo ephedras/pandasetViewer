20e6e31ff627ab6dca0b255a56972bc11c0a94f9
ephedras <114738993+ephedras@users.noreply.github.com>
2024-07-08 16:26:36 +0530

    Add files via upload
    First commit without dataset

---
+++ pandasetViewer.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from pandaset import DataSet
+from pandaset import geometry
+import matplotlib.cm as cm
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import open3d as o3d
+import pandas as pd
+import plotly.graph_objs as go
+from utils.utils import depth_projection,semantic_segmentation,display_map
+
+
+#importing the dataset
+dataset = DataSet('pandaset')
+#listing the data
+allSeqList = dataset.sequences()
+semSeqList = dataset.sequences(with_semseg=True)
+
+
+st.set_page_config(page_title="Pandaset Dataset Viewer",page_icon='assets\logo\icon_clear.png')
+
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+# Set up the sidebar with a logo, a dropdown, and radio buttons
+st.sidebar.image("assets\logo\logo_clear.png", use_column_width=True)
+
+
+dropdown_options = ["Depth Projection", "Semantic Segmentation", "Map Route"]
+
+
+# Radio buttons in the main area
+radio_selection = st.sidebar.radio("Choose an option:", dropdown_options)                                                 
+
+if radio_selection=="Semantic Segmentation":
+    selected_seq = st.sidebar.selectbox("Select an option:", semSeqList)
+else:
+    selected_seq = st.sidebar.selectbox("Select an option:", allSeqList)
+
+
+seq = dataset[selected_seq]
+seq.load()
+
+if radio_selection!="Map Route":
+    cameraSelected = st.sidebar.selectbox("Select Camera:", seq.camera.keys())
+    lidar = seq.lidar
+# timeSlider = st.sidebar.slider("Time Sequence",0, len(lidar.data))
+
+
+
+if st.sidebar.button("Load Data", 'Load'):
+    progress_bar = st.progress(0)
+    
+    
+    if  radio_selection=="Map Route":
+            st.header(f"Map Route")
+            st.subheader(f"For Seq. {selected_seq}")
+            progress_bar.progress(10)
+            display_map(seq)
+    else:
+        totlen = len(lidar.data)
+        if radio_selection=="Depth Projection":
+            # Create tabs for each time step
+            tabs = st.tabs([f"TimeStep {i+1}" for i in range(totlen)])
+            for time in range(totlen):
+                with tabs[time]:
+                    st.header(f"Depth Projection ")
+                    st.subheader(f"For Seq. {selected_seq} at step {time + 1}")
+                    depth_projection(seq,lidar,cameraSelected,time)
+                    progress_bar.progress((time + 1) / totlen)
+        elif radio_selection=="Semantic Segmentation":
+            # Create tabs for each time step
+            tabs = st.tabs([f"TimeStep {i+1}" for i in range(totlen)])
+            for time in range(totlen):
+                with tabs[time]:
+                    st.header(f"Semantic Segmentation ")
+                    st.subheader(f"For Seq. {selected_seq} at step {time + 1}")
+                    semantic_segmentation(seq, lidar, cameraSelected, time)
+                    progress_bar.progress((time + 1) / totlen)  
+         
+                
+    progress_bar.empty()  # Clear the progress bar
+
+with st.sidebar.popover("About"):
+    
+     st.image("assets\logo\logo_clear.png", width=100)
+     st.markdown("Hello World 👋")
+     st.write('''
+This is just the viewer app for **pandaset dataset** used for autonomous vehicle research.
+
+Data : https://www.kaggle.com/datasets/usharengaraju/pandaset-dataset/data. 
+
+Devkit : https://github.com/scaleapi/pandaset-devkit.''')
+     st.info('''
+:warning:   This app is purely designed for academic purposes and the licence attached to the original dataset and devkit upholds             
+Ver: 1.0.13             
+Date: 08/07/2024
+             ''') 
---
+++ utils/utils.py
@@ -0,0 +1,88 @@
+import streamlit as st
+from pandaset import geometry
+import matplotlib.cm as cm
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import folium
+
+
+def depth_projection(seq, lidar, camera_name, seq_idx):
+    points3d_lidar_xyz = lidar.data[seq_idx].to_numpy()[:, :3]
+    choosen_camera = seq.camera[camera_name]
+    projected_points2d, camera_points_3d, inner_indices = geometry.projection(
+        lidar_points=points3d_lidar_xyz,
+        camera_data=choosen_camera[seq_idx],
+        camera_pose=choosen_camera.poses[seq_idx],
+        camera_intrinsics=choosen_camera.intrinsics,
+        filter_outliers=True
+    )
+
+    # Image before projection
+    ori_image = seq.camera[camera_name][seq_idx]
+    st.image(ori_image, caption='Original Image', use_column_width=True)
+
+    # Image after projection
+    distances = np.sqrt(np.sum(np.square(camera_points_3d), axis=-1))
+    colors = cm.jet(distances / np.max(distances))
+
+    fig, ax = plt.subplots()
+    ax.imshow(ori_image)
+    ax.scatter(projected_points2d[:, 0], projected_points2d[:, 1], color=colors, s=1)
+    ax.axis('off')
+    st.pyplot(fig)
+
+def semantic_segmentation(seq, lidar, camera_name, seq_idx):
+    points3d_lidar_xyz = lidar.data[seq_idx].to_numpy()[:, :3]
+    choosen_camera = seq.camera[camera_name]
+    projected_points2d, camera_points_3d, inner_indices = geometry.projection(
+        lidar_points=points3d_lidar_xyz,
+        camera_data=choosen_camera[seq_idx],
+        camera_pose=choosen_camera.poses[seq_idx],
+        camera_intrinsics=choosen_camera.intrinsics,
+        filter_outliers=True
+    )
+    # Image before projection
+    ori_image = seq.camera[camera_name][seq_idx]
+    st.image(ori_image, caption='Original Image', use_column_width=True)
+
+    # Load semantic segmentation
+    semseg = seq.semseg[seq_idx].to_numpy()
+
+    # Get semantic segmentation on image by filtering outside points
+    semseg_on_image = semseg[inner_indices].flatten()
+
+    # Randomly generate colors for semantic segmentation
+    max_seg_id = np.max(semseg_on_image)
+    color_maps = [(random.random(), random.random(), random.random()) for _ in range(max_seg_id + 1)]
+    colors = np.array([color_maps[seg_id] for seg_id in semseg_on_image])
+
+    fig, ax = plt.subplots()
+    ax.imshow(ori_image)
+    ax.scatter(projected_points2d[:, 0], projected_points2d[:, 1], color=colors, s=1)
+    ax.axis('off')  # Remove axis labels and marks
+    st.pyplot(fig)
+
+def display_map(seq):
+    seq.load_gps()
+    
+    lats = [x['lat'] for x in seq.gps]
+    longs = [x['long'] for x in seq.gps]
+    
+    mean_lat = lats[len(lats)//2]
+    mean_long = longs[len(longs)//2]
+    
+    # Create a folium map centered around the mean lat/long
+    folium_map = folium.Map(location=[mean_lat, mean_long], zoom_start=18)
+    
+    # Add the GPS points to the map
+    folium.PolyLine(list(zip(lats, longs)), color='cornflowerblue', weight=10).add_to(folium_map)
+    
+    # Save the map to an HTML file
+    folium_map.save(f'map.html')
+    
+    # Read the HTML file and display it in Streamlit
+    with open(f'map.html', 'r') as f:
+        html_string = f.read()
+    
+    st.components.v1.html(html_string, width=700, height=500)
